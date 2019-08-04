82fcdff4547197a58263d937443cef69a469c3d1
theriley106 <christopherlambert106@gmail.com>
2019-08-04 17:09:04 -0400

    Added setup.sh script

---
+++ setup.sh
@@ -0,0 +1,4 @@
+wget https://github.com/klep/scanline/blob/master/build/Products/Debug/scanline?raw=true -O scanline
+
+echo "Please download the unrotate script from http://www.fmwconcepts.com/imagemagick/unrotate/index.php"
+read -p "\nPress enter when complete"
